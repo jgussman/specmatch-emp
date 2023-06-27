953b1abf1a60e009b01cb323b8fac58580a87d90
jgussman <jgussman@iu.edu>
2023-06-27 14:55:45 -0400

    replace wget with requests library.

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
-import os, sys
-from setuptools import setup, find_packages
+import os
+import sys
+
+import requests
+from setuptools import find_packages, setup
+
+
+def download_file(url, outfile):
+    """
+    Download a file from a url and save it to outfile
+
+    :param url: url of file to download
+    :type url: str
+    :param outfile: path to save file to
+    :type outfile: str
+    """
+    r = requests.get(url, stream=True)
+    with open(outfile, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 shift_only = False
@@ -38,10 +58,11 @@ else:
         },
     )
 
+    HOMEDIR = os.environ['HOME']
+    SPECMATCHDIR = "{0}/.specmatchemp/".format(HOMEDIR)
+
     # download library
     if not shift_only:
-        HOMEDIR = os.environ['HOME']
-        SPECMATCHDIR = "{0}/.specmatchemp/".format(HOMEDIR)
         if not os.path.exists(SPECMATCHDIR):
             os.mkdir(SPECMATCHDIR)
 
@@ -57,30 +78,30 @@ else:
             else:  # total size is unknown
                 sys.stderr.write("read %d\n" % (readsofar,))
 
-        # liburl = "https://zenodo.org/record/60225/files/library.h5"
-        liburl = "https://www.dropbox.com/s/po0kzgjn1j9ha2v/library.h5#"
         LIBPATH = os.path.join(SPECMATCHDIR+"library.h5")
         if not os.path.exists(LIBPATH):
-            os.system("wget --no-check-certificate --output-document=${HOME}/.specmatchemp/library.h5 https://www.dropbox.com/s/po0kzgjn1j9ha2v/library.h5#")
+            download_file("https://www.dropbox.com/s/po0kzgjn1j9ha2v/library.h5#", LIBPATH)
 
     # Additional csv files
-    csv_urls = ["https://www.dropbox.com/s/8wv38eb8dzg1ou2/hires_telluric_mask.csv#",
-                "https://www.dropbox.com/s/ugqzpux73mgjuyj/detrend.csv#",
-                "https://www.dropbox.com/s/lxz0cb57a8xvsfl/uncertainties.csv#"]
-    for f in csv_urls:
-        outfile = os.path.join(SPECMATCHDIR,f.split('/')[-1][:-1])
-        cmd = "wget --no-check-certificate {} -O {}".format(f,outfile)
-        os.system(cmd)
+    csv_url_list = ["https://www.dropbox.com/s/8wv38eb8dzg1ou2/hires_telluric_mask.csv#",
+                    "https://www.dropbox.com/s/ugqzpux73mgjuyj/detrend.csv#",
+                    "https://www.dropbox.com/s/lxz0cb57a8xvsfl/uncertainties.csv#"]
+    for csv_url in csv_url_list:
+        outfile = os.path.join(SPECMATCHDIR,
+                               csv_url.split('/')[-1][:-1]
+                               )
+        download_file(csv_url, outfile)
 
     specdir = os.path.join(SPECMATCHDIR, 'shifted_spectra')
     if not os.path.exists(specdir):
         os.mkdir(specdir)
         # download references
-        ref_urls = ["https://www.dropbox.com/s/4aygfh3qnmorws1/nso_adj.fits#",
+        ref_url_list = ["https://www.dropbox.com/s/4aygfh3qnmorws1/nso_adj.fits#",
                     "https://www.dropbox.com/s/sgkqhwe2kfm9yvp/j26.532_adj.fits#",
                     "https://www.dropbox.com/s/lthwgat0e2s4gqu/j72.718_adj.fits#",
                     "https://www.dropbox.com/s/vqqjlcnw7duuq54/j59.1926_adj.fits#"]
-        for ref in ref_urls:
-            outfile = os.path.join(specdir,ref.split('/')[-1][:-1])
-            cmd = "wget --no-check-certificate {} -O {}".format(ref,outfile)
-            os.system(cmd)
+        for ref_url in ref_url_list:
+            outfile = os.path.join(specdir,
+                                   ref_url.split('/')[-1][:-1]
+                                   )
+            download_file(ref_url, outfile)
