5908db4a27d58841b0e55fa8b8fabc37df9123c4
jgussman <jgussman@iu.edu>
2023-07-18 20:26:21 -0500

    #3 remove normilization during shifting

--- setup.py
+++ setup.py
@@ -41,13 +41,8 @@ else:
         version="0.3",
         packages=find_packages(),
         install_requires=[
-            "numpy",
-            "scipy",
-            "matplotlib",
             "h5py",
-            "pandas",
             "lmfit",
-            "astropy",
             "astroquery",
             "isochrones",
         ],
